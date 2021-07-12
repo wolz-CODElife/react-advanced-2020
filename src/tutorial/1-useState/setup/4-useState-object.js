a6a69793d0cd6c8464f931fc4ca83d9c8b1509e2
wolz-CODElife <joeladewole3@gmail.com>
2021-07-12 11:57:57 +0100

    Usestate counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message')
+  const [person, setPerson] = useState({name: name, age: age, message: message})
+  const changeMessage = () => {
+    if (person.message === 'random message') {      
+      setPerson({...person, message: 'Hello World'})
+    }
+    else{      
+      setPerson({...person, message: 'random message'})
+    }
+  }
+  return (
+    <>
+      <h1>{name}</h1>
+      <h3>{age}</h3>
+      <h4>{message}</h4>
+      <button className="btn" onClick={changeMessage}>Change Message</button>
+    </>
+    )
 };
 
 export default UseStateObject;
