2198f60b2bec17fe68dd7546c206dd5df64df301
wolz-CODElife <joeladewole3@gmail.com>
2021-07-15 23:15:30 +0100

    useEffect short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,19 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  return <>
+    {/* <h1>{firstValue}</h1>
+    <h1>value: {secondValue}</h1> */}
 
-  return <h2>short circuit</h2>;
+    <h1>{text || 'John Doe'}</h1>
+    <button className="btn" onClick={() => setIsError(!isError)}>Toggle error</button>
+    {isError && <h1>Error . . .</h1> }
+    {isError ? <p>There is an Error . . .</p> : <p>There is no Error . . . </p> }
+  </>;
 };
 
 export default ShortCircuit;
