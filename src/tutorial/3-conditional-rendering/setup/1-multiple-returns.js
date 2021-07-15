4671369af442915c28d48d47b33d5bd04543c5df
wolz-CODElife <joeladewole3@gmail.com>
2021-07-15 20:18:03 +0100

    useEffect multiple rendering

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,48 @@
 import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+const url = 'https://api.github.com/users/QuincyLarsons';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('Default User')
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(url)
+      .then((resp) => {
+        if(resp.status >= 200 && resp.status <= 299) {
+          return resp.json()
+        }
+        else{
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((user) => {
+        const {login} = user
+        setUser(login)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        setIsError(true)
+      })
+  }, [])
+
+  if(isLoading) {
+    return (
+      <div>
+        <h2>loading . . .</h2>
+      </div>
+    )
+  }
+  if(isError) {
+    return (
+      <div>
+        <h2>Error found</h2>
+      </div>
+    )
+  }
+  return <h2>{user}</h2>;
 };
 
 export default MultipleReturns;
