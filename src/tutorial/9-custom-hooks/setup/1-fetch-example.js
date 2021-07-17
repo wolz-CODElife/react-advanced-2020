1b7f7963b03c4911b369714b1cb2143c877d1911
wolz-CODElife <joeladewole3@gmail.com>
2021-07-17 23:11:37 +0100

    Custom hooks

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -4,26 +4,40 @@ import { useFetch } from './2-useFetch'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
-
+ 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  const {loading, products} = useFetch(url)
   console.log(products)
   return (
-    <div>
-      <h2>{loading ? 'loading...' : 'data'}</h2>
-    </div>
+    <>
+      <h3>Products</h3>
+      {loading ? <h4>loading...</h4> : 
+        <article className="users">
+          {products.map((product) => {
+            const {name, company, image, price} = product.fields
+            return(
+              <section key={product.id} className="item">
+                {image.map((img) => {
+                  const {id, thumbnails, filename, url} = img
+                  return (
+                    <a key={id} href={url} target="_blank" rel="noopener noreferrer">
+                      <img src={thumbnails.small.url} alt={filename} />
+                    </a>
+                  )
+                })}
+                <div>                  
+                  <h4>
+                    {name}
+                    <p>${price}</p>
+                  </h4>
+                  <p>{company}</p>
+                </div>
+              </section>  
+            )}
+          )}
+        </article>
+      }
+    </>
   )
 }
 
