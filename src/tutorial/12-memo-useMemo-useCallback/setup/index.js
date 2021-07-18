3b956c5109c2a5b5fb1a614c5c146c0cdf0f8168
wolz-CODElife <joeladewole3@gmail.com>
2021-07-18 17:23:54 +0100

    memo - useMemo

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -6,33 +6,58 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  return data.reduce((total, item) => {
+    const price = item.fields.price
+    if (price >= total) {
+      total = price
+    }
+    return total
+  }, 0)/100
+}
 
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [cart, setCart] = useState(0)
 
+  const addToCart = useCallback(() => {
+    setCart(cart + 1)
+  }, [cart])
+
+  const mostExpensive = useMemo(() => calculateMostExpensive(products), [products])
   return (
     <>
       <h1>Count : {count}</h1>
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem"}}>
+        Cart : {cart}
+      </h1>
+      <h1>Most Expensive : ${mostExpensive}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
+  useEffect(() => {
+    console.log('big list called')
+  })
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart} ></SingleProduct>
       })}
     </section>
   )
-}
+})
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
+  useEffect(() => {
+    console.count('single item called')
+  })
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +67,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button className="btn" onClick={() => addToCart()} >Add to cart</button>
     </article>
   )
 }
