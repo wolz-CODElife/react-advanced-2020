31fcd62efc43b7ebbf9c16f85b1cc3324f4b48ef
wolz-CODElife <joeladewole3@gmail.com>
2021-07-18 13:47:17 +0100

    React router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+  const [name, setName] = useState('Default name')
+  const {id} = useParams()
+
+  useEffect(() => {
+    const newPerson = data.find((person) => person.id === parseInt(id))
+    setName(newPerson.name)
+  }, [])
   return (
     <div>
-      <h2>person</h2>
+      <h1>{name}</h1>
     </div>
   );
 };
